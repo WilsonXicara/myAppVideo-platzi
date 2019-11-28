713d21df4185cad05b29ee15f8b238435c19f57f
WilsonXicara <wilsonx192@gmail.com>
2019-11-28 16:53:16 -0600

    Instalación de React y ReactDOM + corridas en modo desarrollo (ejecutando 'npm run build:dev')

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('HOLA MUNDO!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// console.log('HOLA MUNDO!')
+
+const CONTAINER = document.getElementById('app');
+
+// ReactDOM.render(QUE-SE-RENDERIZA, DÓNDE-SE-RENDERIZA);
+const holaMundo = <h1>Hola Mundo Programación en ReactJS</h1>;
+ReactDOM.render(holaMundo, CONTAINER);
