a0ced08f19e9c9c998b72ae56ba3dcaea10b21bb
WilsonXicara <wilsonx192@gmail.com>
2019-11-29 16:23:35 -0600

    Manejo de formularios en componentes

--- src/categories/components/categories.js
+++ src/categories/components/categories.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // Components
 import Category from './category';
+import Search from '../../widgets/containers/search';
 // Styles
 import './categories.css';
 
 function Categories(props) {
     return (
         <div className="Categories">
+            <Search />
             {
                 props.categories.map(category => {
                     return (
---
+++ src/widgets/components/search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// Styles
+import './search.css';
+
+// function Search(props) {
+//     return (
+//         <h1></h1>
+//     )
+// }
+
+const Search = () => (
+    <form action="" className="Search">
+        <input type="text" name="" id="" className="Search-input" placeholder="Busca tu video favorito" />
+    </form>
+)
+
+export default Search;
