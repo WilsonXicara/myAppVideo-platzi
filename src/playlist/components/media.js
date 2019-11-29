f6c4fe33e8c97d0e4b581ee9b8eb215dbf26fbd1
WilsonXicara <wilsonx192@gmail.com>
2019-11-29 09:28:22 -0600

    Inline Style y estilos desde archivo css

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,15 +1,34 @@
 // Esta importación es posible gracias a la habilidad de desestructuración de JavaScript
 import React, { Component } from 'react';
+import './media.css';
 
 class Media extends Component {
     render() {
         // Similar al constructor
+        //
+        // Convenciones:
+        // - Por convención, se utiliza escritura camelCase para atributos CSS
+        //      font-size: 14px;        ->  fontSize: 14
+        //      background-color: red;  ->  backgroundColor: 'red'
+        // - Por defecto, si la medida es en 'px' puede omitirse el sufijo; si es diferente, se especifica entre ''
+        //
+        // Style inline
+        const styles = {
+            container: {
+                fontSize: 24,
+                // backgroundColor: 'red',
+                color: '#44546b',
+                cursor: 'pointer',
+                width: 260,
+                border: '1px solid red'
+            }
+        }
         return (
-            <div>
-                <div>
-                    <img src="" alt="" width={260} height={160}/>
-                    <h3>¿Por qué aprender React?</h3>
-                    <p>WiilsonXicará</p>
+            <div className="Media">
+                <div className="Media-cover">
+                    <img className="Media-image" src="./images/covers/bitcoin.jpg" alt="" width={260} height={160}/>
+                    <h3 className="Media-title">¿Por qué aprender React?</h3>
+                    <p className="Media-author">WiilsonXicará</p>
                 </div>
             </div>
         )
