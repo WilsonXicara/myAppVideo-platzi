a739ca8b34e5fe224ca1ab54679aa5dd0930d6b0
WilsonXicara <wilsonx192@gmail.com>
2019-12-02 10:35:34 -0600

    Media events, obtención de la duración del video

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -4,10 +4,13 @@ import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
 import PlayPause from '../components/play-pause';
+import Timer from '../components/timer';
+import VideoPlayerControls from '../components/video-player-controls';
 
 class VideoPlayer extends Component {
     state = {
         pause: true,
+        duration: 0,
     }
     togglePlay = (event) => {
         this.setState({
@@ -19,14 +22,25 @@ class VideoPlayer extends Component {
             pause: !this.props.autoplay
         })
     }
+    handleLoadedMetadata = event => {
+        this.video = event.target;  // Quien dispara el evento
+        this.setState({
+            duration: this.video.duration
+        })
+    }
     render() {
         return (
             <VideoPlayerLayout>
                 <Title title="Esto es un video chido!"/>
-                <PlayPause pause={this.state.pause}
-                           handleClick={this.togglePlay} />
+                <VideoPlayerControls>
+                    <PlayPause pause={this.state.pause}
+                               handleClick={this.togglePlay} />
+                    <Timer duration={this.state.duration} />
+                </VideoPlayerControls>
+                
                 <Video autoplay={this.props.autoplay}
                        pause={this.state.pause}
+                       handleLoadedMetadata={this.handleLoadedMetadata}
                        src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4" />
             </VideoPlayerLayout>
         )
